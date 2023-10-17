59ccdebbcdc94fe1694775d4d5048f9db1bc64ed
joaog <joaoglproenca@gmail.com>
2023-10-16 22:46:28 -0300

    Primeiro commit da conexão DB

---
+++ conect.php
@@ -0,0 +1,14 @@
+<?php
+
+$pdo= null;
+
+    try {
+        $pdo= new PDO('mysql:host=localhost;dbname=databasear','root', '');
+    } catch (Exception $e) {
+        echo $e->getMessage(); 
+        die();
+    }
+
+var_dump($pdo);
+
+?>
