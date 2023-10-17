8a334d62de71243765204be8f9024519f24dfe74
joaog <joaoglproenca@gmail.com>
2023-10-17 17:31:29 -0300

    subindo o arquivo que chama a conecxão e escreve valores em tela da tabela produto

--- chamada.php
+++ chamada.php
@@ -1,4 +1,13 @@
 <?php
 
+$pdo = require 'conect.php';
 
+$sql = 'select * from produto';
+
+echo "<h3> Produtos</h3>";
+
+foreach($pdo->query($sql) as $key=> $value)
+{
+    echo 'id' . $value['id'] . '<br> Descricao' . $value['descricao'] . '<hr>' ; 
+}
 ?>
